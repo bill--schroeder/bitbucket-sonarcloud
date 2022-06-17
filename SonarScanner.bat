f3e7103eff478c592cc8def5660212c614293f94
Bill Schroeder <bill@zealocity.com>
2022-06-17 09:53:20 -0500

    Update SonarScanner.bat

--- SonarScanner.bat
+++ SonarScanner.bat
@@ -8,8 +8,12 @@ SET PATH=%PATH%;"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild
 SET JAVA_HOME=C:\Apps\SonarScanner\jdk\
 SET SONAR_TOKEN="${SONAR_TOKEN}"
 
+SET BUILD_SOLUTION=%1
+
+
 SonarScanner.MSBuild.exe begin /k:"product_key" /d:sonar.login=%SONAR_TOKEN% /o:"organization_key" /d:"sonar.host.url=https://sonarcloud.io"
 
-MSBuild.exe <path to solution.sln> /t:Rebuild
+REM MSBuild.exe <path to solution.sln or project> /t:Rebuild
+MSBuild.exe %BUILD_SOLUTION%
 
 SonarScanner.MSBuild.exe end /d:sonar.login=%SONAR_TOKEN%
